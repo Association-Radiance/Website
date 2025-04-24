aa3c337b64d7602d4b4ee7d0a12e039de251cd41
Axel <axel.damart@gmail.com>
2025-04-24 17:21:52 +0200

    remove passing env with services.yaml

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -7,22 +7,17 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 
-class MailerService
+readonly class MailerService
 {
-    private string $contact_sender_address;
-    private string $contact_recipient_address;
-
-    public function __construct(private readonly MailerInterface $mailerInterface, string $contact_sender_address, string $contact_recipient_address)
+    public function __construct(private MailerInterface $mailerInterface)
     {
-        $this->contact_sender_address = $contact_sender_address;
-        $this->contact_recipient_address = $contact_recipient_address;
     }
 
     public function sendContactEmail(Contact $contact): array
     {
         $email = (new TemplatedEmail())
-            ->from($this->contact_sender_address)
-            ->to($this->contact_recipient_address)
+            ->from($_ENV['CONTACT_SENDER_ADDRESS'])
+            ->to($_ENV['CONTACT_RECIPIENT_ADDRESS'])
             ->subject("Nouvelle demande de contact de " . $contact->getName())
             ->htmlTemplate('emails/contact.html.twig')
             ->locale('fr')
