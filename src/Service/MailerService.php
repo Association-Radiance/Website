4cca3f9cb47acfae1cc798726205099412393efa
Axel <axel.damart@gmail.com>
2025-04-24 16:25:28 +0200

    add contact mailer sender and recipient to env

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -4,20 +4,28 @@ namespace App\Service;
 
 use App\Entity\Contact;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 
 readonly class MailerService
 {
-    public function __construct(private MailerInterface $mailerInterface)
+
+    private string $contact_sender_address;
+    private string $contact_recipient_address;
+
+    public function __construct(private MailerInterface $mailerInterface, string $contact_sender_address, string $contact_recipient_address)
     {
+        $this->contact_sender_address = $contact_sender_address;
+        $this->contact_recipient_address = $contact_recipient_address;
     }
 
     public function sendContactEmail(Contact $contact): array
     {
         $email = (new TemplatedEmail())
-            ->to('axel.damart@hotmail.fr')
-            ->subject("test")
+            ->from($this->contact_sender_address)
+            ->to($this->contact_recipient_address)
+            ->subject("Nouvelle demande de contact de " . $contact->getName())
             ->htmlTemplate('emails/contact.html.twig')
             ->locale('fr')
             ->context(['contact' => $contact]);
