bba2bd310b7c0fe9c41680bff0281d3f4cfa67e2
Axel Damart <axeldamart@pop-os.home>
2026-08-11 16:22:56 +0200

    handle edit donation

--- src/Controller/DonationController.php
+++ src/Controller/DonationController.php
@@ -7,7 +7,6 @@ use App\Form\DonationType;
 use App\Repository\DonationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,8 +14,6 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route("/donations")]
 final class DonationController extends AbstractController
 {
-    public function __construct(private readonly DonationRepository $donationRepository, private readonly EntityManagerInterface $entityManager) {}
-
     #[Route("/new", name: "donation_new", methods: ["GET", "POST"])]
     public function new(Request $request, EntityManagerInterface $entityManager, DonationRepository $donationRepository): Response
     {
@@ -46,6 +43,35 @@ final class DonationController extends AbstractController
         return $this->render("donation/_form.html.twig", [
             "form" => $form,
             "title" => "Nouvelle donation",
+        ]);
+    }
+
+    #[Route("/{id}/edit", name: "donation_edit", methods: ["GET", "POST"])]
+    public function edit(Donation $donation, Request $request, EntityManagerInterface $entityManager, DonationRepository $donationRepository): Response
+    {
+        $form = $this->createForm(DonationType::class, $donation, [
+            "action" => $this->generateUrl("donation_edit", ["id" => $donation->getId()]),
+            "method" => "POST",
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $response = $this->render("donation/success.stream.html.twig", [
+                "donations" => $donationRepository->findAll(),
+                "total" => $donationRepository->getTotalDonation(),
+            ]);
+
+            $response->headers->set("Content-Type", "text/vnd.turbo-stream.html");
+
+            return $response;
+        }
+
+        return $this->render("donation/_form.html.twig", [
+            "form" => $form,
+            "title" => "Modifier la donation",
         ]);
     }
 }
